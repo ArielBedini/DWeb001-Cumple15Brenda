1438d3702ebe9f990846b97cf1de628c583dc470
Ariel Bedini <arielbedini@gmail.com>
2023-04-11 17:43:01 -0300

    Aprendiendo a modularizar SASS con varias hojas de estilo aplicando la sintaxis anterior

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,21 @@
-const {src, dest} = require("gulp");
+const {src, dest, watch} = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 function css(done) {
     src("src/scss/app.scss")
         .pipe(sass())
-        .pipe(dest("build/css"))
-    
+        .pipe(dest("build/css"));    
+    done();
+}
+
+// función que esta escuchando cada cambio en lo arhcivos scss
+function dev(done) {
+    watch("src/scss/app.scss", css);
+
     done();
 }
 
 exports.css = css;
+exports.dev = dev;
 
 

eb9aa053f7685c23a1a6292227f98a98ec095244
Ariel Bedini <arielbedini@gmail.com>
2023-04-25 02:03:34 -0300

    Agregamos los formularios para ingreso de preferencias del usuario

--- build/js/app.js
+++ build/js/app.js
@@ -1,1 +1,33 @@
-console.log("saludos a todos");
+document.addEventListener('DOMContentLoaded',function() {
+
+});
+
+function iniciarApp() {
+
+}
+
+function formularioRSVP(mostrar) {
+    let formulario = document.querySelector(".formularioRSVP");
+    let boton = document.querySelector(".botonEnlace-rsvp");
+    
+    if(mostrar){
+        formulario.style.display = "flex";
+        boton.style.display = "none";
+    } else {
+        formulario.style.display = "none";
+        boton.style.display = "flex";
+    }
+}
+
+function formularioMusica(mostrar) {
+    let formulario = document.querySelector(".formularioMusica");
+    let boton = document.querySelector(".botonEnlace-musica");
+    
+    if(mostrar){
+        formulario.style.display = "flex";
+        boton.style.display = "none";
+    } else {
+        formulario.style.display = "none";
+        boton.style.display = "flex";
+    }
+}
